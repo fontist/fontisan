4e41c45a677329ae41365c8168022c1bde7c8f7d
Ronald Tse <ronald.tse@ribose.com>
2025-12-27 21:15:21 +0800

    fix: ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,22 @@ require "tempfile"
 # Define the fixtures directory constant for test files
 FIXTURES_DIR = File.join(__dir__, "fixtures")
 
+# Check if test fixtures exist, download if missing
+REQUIRED_FIXTURES = [
+  File.join(FIXTURES_DIR, "fonts/NotoSans-Regular.ttf"),
+  File.join(FIXTURES_DIR, "fonts/libertinus/Libertinus-7.051/static/OTF/LibertinusSerif-Regular.otf"),
+  File.join(FIXTURES_DIR, "fonts/MonaSans/MonaSans/variable/MonaSans[wdth,wght].ttf"),
+  File.join(FIXTURES_DIR, "fonts/NotoSerifCJK/NotoSerifCJK.ttc"),
+  File.join(FIXTURES_DIR, "fonts/NotoSerifCJK-VF/Variable/OTC/NotoSerifCJK-VF.otf.ttc"),
+].freeze
+
+unless REQUIRED_FIXTURES.all? { |f| File.exist?(f) }
+  warn "Test fixtures not found. Downloading..."
+  require "rake"
+  Rake.application.rake_require "Rakefile", [File.expand_path("../../Rakefile", __dir__)]
+  Rake::Task["fixtures:download"].invoke
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
